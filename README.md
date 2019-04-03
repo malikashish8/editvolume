1c5f7d5726fefcb1bbb7394e9aaf8dacbaf4b66b
Ashish Malik <malikashish8@gmail.com>
2019-04-03 21:50:07 +1100

    WIP

--- README.md
+++ README.md
@@ -5,11 +5,19 @@ Edit contents of a docker volume on host in real time using tools/editors instal
 ## Usage
 Run `editvolume` by mounting the **volume** to be edited and the **host folder** on which contents of volume will available:
 ```bash
-docker run --rm -e "EDITUSER=`id -u`" -v <volume_to_edit>:/volume -v <host_folder>:/folder malikashish8/editvolume
+docker run --rm \
+    -e "EDITUSER=`id -u`" \
+    -v <volume_to_edit>:/volume \
+    -v <host_folder>:/folder \
+    malikashish8/editvolume
 ```
 example:
 ```bash
-docker run --rm -e "EDITUSER=`id -u`" -v jenkins_config:/volume -v ~/jenkins_config:/folder malikashish8/editvolume
+docker run --rm \
+    -e "EDITUSER=`id -u`" \
+    -v jenkins_config:/volume \
+    -v ~/jenkins_config:/folder \
+    malikashish8/editvolume
 ```
 > EDITUSER is required to change permission of copied files to host user
 
--- sync.sh
+++ sync.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/evn sh
 
+[[ ! -z ${DEBUG} ]] && set -x
+
 usage() {
     echo "usage sample:"
     echo "docker run --rm -e \"EDITUSER=\`id -u\`\" -v <volume_to_edit>:/volume -v <host_folder>:/folder editvolume"
@@ -7,21 +9,29 @@ usage() {
 
 [[ -z ${EDITUSER} ]] && echo EDITUSER parameter is required && usage && exit 1
 
-echo "Copying files from volume to local folder"
-rsync --delete -r /volume/ /folder/ 1>/dev/null
+echo "copying files from volume to local folder"
+rsync --delete --recursive /volume/ /folder/ 1>/dev/null
 
-echo "Userid $EDITUSER"
-chown -R "$EDITUSER:$EDITUSER" /folder
-echo "Copy complete"
+chown --recursive "$EDITUSER:$EDITUSER" /folder
+echo "copy complete"
 
 notify_folder() {
-    inotifywait -r -m -e modify -e move -e create -e delete /folder 2>/dev/null
+    inotifywait --recursive --monitor --event modify,move,create,delete /folder 2>/dev/null
 }
 
-notify_folder | tee /dev/stderr | while read line; do echo 'update folder->volume' && rsync --delete -r /folder/ /volume/; done &
+notify_folder | tee /dev/stderr | while read line
+do 
+    echo 'update folder -> volume'
+    rsync --delete --update --recursive /folder/ /volume/
+done 2>&1 &
 
 notify_volume() {
-    inotifywait -r -m -e modify -e move -e create -e delete /volume 2>/dev/null
+    inotifywait --recursive --monitor --event modify,move,create,delete /volume 2>/dev/null
 }
 
-notify_volume | while read line; do echo 'update volume->folder' && rsync --delete -r /volume/ /folder/; done
+notify_volume | tee /dev/stderr | while read line
+do 
+    echo 'update volume -> folder'
+    rsync --delete --update --recursive /volume/ /folder/
+    chown --recursive "$EDITUSER:$EDITUSER" /folder
+done 2>&1
