2cc6f5ef417a92a3c24649b2054099c71683cbc4
Ashish Malik <malikashish8@gmail.com>
2019-04-04 15:25:07 +1100

    process control to fix bidirectional sync issues

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# editvolume
+# malikashish8/editvolume
 [![Build Status](https://travis-ci.com/malikashish8/editvolume.svg?branch=master)](https://travis-ci.com/malikashish8/editvolume)
 
-Edit contents of a docker volume on host in real time using tools/editors installed on host. This is useful for debugging running containers, editing the config files in a volume etc. Changes are automatically detected and files/folder are synced.
+Edit contents of a docker volume on host in real time using tools/editors installed on the host. This is useful for debugging running containers, editing the config files in a volume, development and testing with docker etc. Changes are automatically detected and files/folder are synced.
 
 ## Usage
 Run `editvolume` by mounting the **volume** to be edited and the **host folder** on which contents of volume will be available:
@@ -12,15 +12,41 @@ docker run --rm -it \
     -v <host_folder>:/folder \
     malikashish8/editvolume
 ```
-example:
+### Usage Sample:
 ```bash
-docker run --rm -it \
+dev@ubu:~$ docker run --rm -it \
     -e "HOSTUSER=`id -u`" \
     -v jenkins_config:/volume \
     -v ~/jenkins_config:/folder \
     malikashish8/editvolume
+copying 996.0K from volume to host folder
+copy complete
+folder -> volume /folder/ CREATE plugins.config
+folder -> volume /folder/ MODIFY plugins.config
+folder -> volume /folder/ MODIFY plugins.config
+folder -> volume /folder/ MODIFY plugins.config
+folder -> volume /folder/ CREATE config_notes.md
+folder -> volume /folder/ MODIFY config_notes.md
+folder -> volume /folder/ MOVED_FROM config_notes.md
+folder -> volume /folder/ MOVED_TO user_notes.md
+folder -> volume /folder/ MOVED_FROM user_notes.md
+volume -> folder /volume/ CREATE jenkins.log
+volume -> folder /volume/ MODIFY jenkins.log
+^Cshutting down
 ```
 > `HOSTUSER` is required to change ownership of copied files to host user
 
 
 Files are synced as long as __editvolume__ is run. To constantly sync files run it in detached (`-d`) mode.
+
+## Features
+* Realtime bi-directional recursive sync between docker volume and folder on host
+    * `rsync` used for copying changes
+    * `inotifywait` used to detect changes
+* User permissions are set automatically:
+    * Ownership of Volume files set to volume folder user
+    * Ownership of Host Folder files set to current user on host
+* Light weight - based on alpine
+
+## Caveat
+* Contents of _host folder_ are edited to begin with so inadvertent updates are not made to the volume.
--- sync.sh
+++ sync.sh
@@ -11,7 +11,7 @@ cleanup() {
 
 usage() {
     echo "usage sample:"
-    echo "docker run --rm -it -e \"HOSTUSER=\`id -u\`\" -v <volume_to_edit>:/volume -v <host_folder>:/folder editvolume"
+    echo "docker run --rm -it -e \"HOSTUSER=\`id -u\`\" -v <volume_to_edit>:/volume -v <host_folder>:/folder malikashish8/editvolume"
 }
 
 [[ -z ${HOSTUSER} ]] && echo HOSTUSER parameter is required && usage && exit 1
@@ -26,27 +26,47 @@ fi 1>/dev/null
 echo "copying `du -h -d0 /volume | cut -f1` from volume to host folder"
 rsync --delete --owner --group --chown="$HOSTUSER:$HOSTUSER" --recursive /volume/ /folder/ 1>/dev/null
 
-#chown --recursive "$HOSTUSER:$HOSTUSER" /folder
 echo "copy complete"
 
 notify_folder() {
     inotifywait --recursive --monitor --event modify,move,create,delete /folder 2>/dev/null
 }
+sync_volume() {
+    notify_folder | while read line
+    do
+        echo "folder -> volume $line"
+        # Switch to one directional sync to avoid stepping over ourself
+        ps>/tmp/f1
+        FOLDER_PID=$(cat /tmp/f1 | grep '/volume' | awk 'NR==1 {print $1}')
+        [[ ! -z "$FOLDER_PID" ]] && kill $FOLDER_PID
 
-notify_folder | while read line
-do 
-    echo "folder -> volume $line"
-    rsync --delete --update --owner --group --chown="$VOLUMEUSER:$VOLUMEUSER" --recursive /folder/ /volume/
-    #chown --recursive "$VOLUMEUSER:$VOLUMEGROUP" /volume
-done 2>&1 &
+        rsync --delete --update --owner --group --chown="$VOLUMEUSER:$VOLUMEUSER" --recursive /folder/ /volume/
+        # Switch back to bi-directional sync
+        sync_folder
+    done &
+}
+
 
 notify_volume() {
     inotifywait --recursive --monitor --event modify,move,create,delete /volume 2>/dev/null
 }
+sync_folder() {
+    notify_volume | while read line
+    do 
+        echo "volume -> folder $line"
+        # Switch to one directional sync to avoid stepping over ourself
+        ps>/tmp/f2
+        VOLUME_PID=$(cat /tmp/f2 | grep '/folder' | awk 'NR==1 {print $1}')
+        [[ ! -z "$VOLUME_PID" ]] && kill $VOLUME_PID
+        rsync --delete --update --owner --group --chown="$HOSTUSER:$HOSTUSER" --recursive /volume/ /folder/
+        # Switch back to bi-directional sync
+        sync_volume
+    done &
+}
 
-notify_volume | while read line
-do 
-    echo "volume -> folder $line"
-    rsync --delete --update --owner --group --chown="$HOSTUSER:$HOSTUSER" --recursive /volume/ /folder/
-    #chown --recursive "$HOSTUSER:$HOSTUSER" /folder
-done 
+sync_folder
+sync_volume
+
+# keep the container running
+while true; do sleep 500; done
+
